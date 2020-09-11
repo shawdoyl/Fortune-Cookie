28c57d5e854ced04d6ff74d40884e03f37a3ccc4
Doyle Shaw <shawdoyl@isu.edu>
2020-09-11 08:47:54 -0600

    Create a random wisdom generator

---
+++ FortuneCookie/Module1.vb
@@ -0,0 +1,49 @@
+﻿'Doyle_Shaw
+'RCET0265
+'Fall_2020
+'Fortune Cookie
+'https://github.com/shawdoyl/Fortune-Cookie.git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module Module1
+
+    Sub Main()
+        Dim randomNumber As Integer
+        Dim quitProgram As Boolean = False
+
+        Do While quitProgram = False
+
+            Console.Clear()
+            Randomize()
+            randomNumber = CInt((3 * Rnd()) + 1)
+            Console.WriteLine("Your gift of wisdom is:")
+            Console.WriteLine()
+            If randomNumber = 1 Then
+                Console.WriteLine("All boring normal people wear digital watches.")
+            ElseIf randomNumber = 2 Then
+                Console.WriteLine("Never leave home without a towel.")
+            ElseIf randomNumber = 3 Then
+                Console.WriteLine("You know it is extremely unlikely to be rescued when vented into space.")
+            ElseIf randomNumber = 3 Then
+                Console.WriteLine("Be glad. If the earth is destroyed then the worst poem in the universe is also destroyed along with it.")
+            ElseIf randomNumber = 4 Then
+                Console.WriteLine("The answer to life, meaning and the universe is 42.")
+            Else
+                Console.WriteLine("No fortune for you.")
+            End If
+            Console.WriteLine()
+            Console.WriteLine("If more sage wisdom is desired, then press Enter, otherwise press q.")
+            If Console.ReadLine() = "q" Then
+                quitProgram = True
+            Else
+                quitProgram = False
+            End If
+
+        Loop
+
+    End Sub
+
+End Module
---
+++ FortuneCookie/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FortuneCookie")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FortuneCookie")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3bf6f6f5-a9bb-4e61-984d-301d8c3b65d3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
